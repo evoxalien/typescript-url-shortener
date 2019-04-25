6f2860aa791979aec0b348203565bbc62a6c34e4
evoxalien <>
2019-04-25 11:16:27 -0600

    Updates to publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 export AWS_DEFAULT_PROFILE=johnvenz
 
+echo "Running tsc"
 tsc
 
+echo "Zipping Code"
 rm publish.zip
 cd dist
 zip -X -r ../publish.zip *
 cd ..
-aws lambda update-function-code --function-name urlShortener --zip-file fileb://publish.zip
+
+echo "Deploying Code"
+aws lambda update-function-code --function-name urlShortener --zip-file fileb://./publish.zip
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-
 export const handler = async (event: any = {}): Promise<any> => {
-    console.log('Hello World! Woooo!');
+    console.log('Hello World! 123!');
     const response = JSON.stringify(event, null, 2);
     return response;
 }
